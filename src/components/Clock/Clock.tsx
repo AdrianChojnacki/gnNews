142d168a89ee6658a8a6d285753cb4bb68f21980
Adrian Chojnacki <chojnackiadrian1@gmail.com>
2023-03-30 16:20:43 +0200

    Fixed hydration error

--- src/components/Clock/Clock.tsx
+++ src/components/Clock/Clock.tsx
@@ -3,11 +3,15 @@ import { Box } from "@chakra-ui/react";
 
 export const Clock = () => {
   const [time, setTime] = useState(new Date());
+  const [hydrated, setHydrated] = useState(false);
 
   useEffect(() => {
+    setHydrated(true);
     const stopwatch = setInterval(() => setTime(new Date()), 1000);
     return () => clearInterval(stopwatch);
-  });
+  }, []);
+
+  if (!hydrated) return null;
 
   return <Box>{time.toLocaleTimeString()}</Box>;
 };
